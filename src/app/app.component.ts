6a24af57c1452e9bd6166664f90b4135440458b1
moeabdol <mohd.a.saed@gmail.com>
2017-09-28 08:15:50 +0300

    Create Node interface and add node and link styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import * as d3 from 'd3';
+
+interface Node {
+  name: string;
+  children: Node[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
     console.log('D3.js version:', d3['version']);
+
+    this.collapsibleForceGraph();
+  }
+
+  collapsibleForceGraph() {
   }
 }
